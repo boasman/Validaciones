1e8782b4da0ff0e58ce45f6d1a9a52898403536b
Elvis Boasman <Elvisboasman@gmail.com>
2024-09-15 20:36:39 +0200

    Validaciones de button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DxButtonModule } from 'devextreme-angular';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,DxButtonModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Validaciones';
+
+  helloWorld() {
+    alert('Hello world!');
+}
 }
