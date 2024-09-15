cfcbc14fbc01c399be2196e1106380b6202df2fb
Elvis Boasman <Elvisboasman@gmail.com>
2024-09-15 20:40:26 +0200

    Se agrego el textbox de login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DxButtonModule } from 'devextreme-angular';
+import { DxTextBoxModule, DxValidatorModule } from 'devextreme-angular';
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,DxButtonModule ],
+  imports: [RouterOutlet,DxButtonModule,
+    DxTextBoxModule,
+    DxValidatorModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Validaciones';
+
+  login: string = '';
 
   helloWorld() {
     alert('Hello world!');
